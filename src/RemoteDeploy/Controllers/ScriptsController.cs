ee878ceaea5c80c3593e3263b80478ebcaf6fd1a
xuzhe <xuzhe@q1.com>
2020-04-28 17:42:31 +0800

    Add exec script controller.

--- src/RemoteDeploy/Controllers/ScriptsController.cs
+++ src/RemoteDeploy/Controllers/ScriptsController.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace RemoteDeploy.Controllers
 {
-    [Route("api/commands")]
+    [Route("api/scripts")]
     [ApiController]
-    public class CommandsController : ControllerBase
+    public class ScriptsController : ControllerBase
     {
         [HttpPost]
-        public IActionResult Exec([FromForm]string ip, [FromForm]string rootUser, [FromForm]string rootPassword, [FromForm]string command)
+        public async Task<IActionResult> Exec([FromForm]string ip, [FromForm]string rootUser, [FromForm]string rootPassword, IFormFile file)
         {
-            var result = $"{rootUser}@{ip}:{command}\r\n";
+            var result = $"{rootUser}@{ip}:./{file.FileName}\r\n";
+
+            var directory = Path.Combine("/files", ip);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            var filePath = Path.Combine(directory, file.FileName);
+            await using (var stream = System.IO.File.Create(filePath))
+            {
+                await file.CopyToAsync(stream);
+            }
 
             try
             {
@@ -19,7 +34,10 @@ namespace RemoteDeploy.Controllers
                 Cmd($"mkdir -p /keys/{ip}/sshkey");
                 Cmd($"ssh-keygen -t rsa -b 4096 -f /keys/{ip}/sshkey/id_rsa -P ''");
                 Cmd($"sshpass -p {rootPassword} ssh-copy-id -o \"StrictHostKeyChecking = no\" -o \"UserKnownHostsFile=/dev/null\" -i /keys/{ip}/sshkey/id_rsa.pub {rootUser}@{ip}");
-                result += Cmd($"ssh -q -o \"StrictHostKeyChecking no\" -o \"UserKnownHostsFile=/dev/null\" -i /keys/{ip}/sshkey/id_rsa \"{rootUser}@{ip}\" \"{command}\"");
+                Cmd($"scp -q -o \"StrictHostKeyChecking no\" -o \"UserKnownHostsFile=/dev/null\" -i /keys/{ip}/sshkey/id_rsa -r \"{filePath}\" \"{rootUser}@{ip}:/root/{file.FileName}\"");
+                Cmd($"ssh -q -o \"StrictHostKeyChecking no\" -o \"UserKnownHostsFile=/dev/null\" -i /keys/{ip}/sshkey/id_rsa \"{rootUser}@{ip}\" \"chmod +x /root/{file.FileName}\"");
+                result += Cmd($"ssh -q -o \"StrictHostKeyChecking no\" -o \"UserKnownHostsFile=/dev/null\" -i /keys/{ip}/sshkey/id_rsa \"{rootUser}@{ip}\" \"/root/{file.FileName}\"");
+                Cmd($"ssh -q -o \"StrictHostKeyChecking no\" -o \"UserKnownHostsFile=/dev/null\" -i /keys/{ip}/sshkey/id_rsa \"{rootUser}@{ip}\" \"rm -f /root/{file.FileName}\"");
                 Cmd($"ssh -q -o \"StrictHostKeyChecking no\" -o \"UserKnownHostsFile=/dev/null\" -i /keys/{ip}/sshkey/id_rsa \"{rootUser}@{ip}\" \"rm -f .ssh/authorized_keys\"");
             }
             catch (Exception ex)
